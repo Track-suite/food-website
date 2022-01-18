27a36e0703a2ec8a3329e8dd4d951971c6891c1c
Asram Ahamed <asrama1998@gmail.com>
2022-01-18 14:08:20 +0530

    created about us

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./Pages/Home";
-import Menu from "./Pages/Menu"
+import Menu from "./Pages/Menu";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./Pages/About";
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/menu" exact component={Menu}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/menu" exact component={Menu} />
+          <Route path="/about" exact component={About} />
         </Switch>
       </Router>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
